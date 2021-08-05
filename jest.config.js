ecf3b3bdbbc4b72f43792784742e80109d386d2f
Matheus Alves <matheus.alves@meliuz.com.br>
2021-08-05 17:19:10 -0400

    chore: Aplica as configuraçoes do JEst

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  collectCoverage: true, /** Enable report coverage */
+  coverageReporters: ['text', 'lcov'], /** Types of reports */
 	preset: 'react-native',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"], /** Enable expect tools */
+	roots: ["<rootDir>/src"],
+	transform: {
+		"^.+\\.tsx?$": "ts-jest",
+	},
+	testMatch: [
+		"<rootDir>/src/**/__tests__/*.spec.(ts|tsx)"
+	],
 };
